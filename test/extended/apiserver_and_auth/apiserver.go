ebeff72eb2f0e64ce8eb536f5c633582ba777895
Xingxing Xia <xxia@redhat.com>
2020-11-16 16:33:28 +0800

    Auto OCP-25806 (#215)
    * [rebased on --timeout change] Auto OCP-25806
    
    * Include kube-apiserver
    
    * Remove debugging code

--- test/extended/apiserver_and_auth/apiserver.go
+++ test/extended/apiserver_and_auth/apiserver.go
@@ -1,6 +1,11 @@
 package apiserver_and_auth
 
 import (
+	"time"
+	"regexp"
+	"strconv"
+	"k8s.io/apimachinery/pkg/util/wait"
+
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 
@@ -54,4 +59,106 @@ var _ = g.Describe("[sig-api-machinary] Apiserver_and_Auth", func() {
 			e2e.Logf("#### privileged value: %s ####", msg)
 		}
 	})
+
+	// author: xxia@redhat.com
+	// It is destructive case, will make kube-apiserver roll out, so adding [Disruptive]. One rollout costs about 25mins, so adding [Slow]
+	g.It("Medium-25806-Force encryption key rotation for etcd datastore [Slow][Disruptive]", func() {
+		// only run this case in Etcd Encryption On cluster
+		g.By("Check if cluster is Etcd Encryption On")
+		output, err := oc.WithoutNamespace().Run("get").Args("apiserver/cluster", "-o=jsonpath={.spec.encryption.type}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		if "aescbc" == output {
+			g.By("Get encryption prefix")
+			var err error
+			var oasEncValPrefix1, kasEncValPrefix1 string
+
+			oasEncValPrefix1, err = GetEncryptionPrefix(oc, "/openshift.io/routes")
+			o.Expect(err).NotTo(o.HaveOccurred())
+			e2e.Logf("openshift-apiserver resource encrypted value prefix before test is %s", oasEncValPrefix1)
+
+			kasEncValPrefix1, err = GetEncryptionPrefix(oc, "/kubernetes.io/secrets")
+			o.Expect(err).NotTo(o.HaveOccurred())
+			e2e.Logf("kube-apiserver resource encrypted value prefix before test is %s", kasEncValPrefix1)
+
+			var oasEncNumber, kasEncNumber int
+			oasEncNumber, err = GetEncryptionKeyNumber(oc, `encryption-key-openshift-apiserver-[^ ]*`)
+			kasEncNumber, err = GetEncryptionKeyNumber(oc, `encryption-key-openshift-kube-apiserver-[^ ]*`)
+
+			t := time.Now().Format(time.RFC3339)
+			patchYamlToRestore := `[{"op":"replace","path":"/spec/unsupportedConfigOverrides","value":null}]`
+			// Below cannot use the patch format "op":"replace" due to it is uncertain
+			// whether it is `unsupportedConfigOverrides: null`
+			// or the unsupportedConfigOverrides is not existent
+			patchYaml := `
+spec:
+  unsupportedConfigOverrides:
+    encryption:
+      reason: force OAS rotation ` + t
+			for _, kind := range []string{"openshiftapiserver", "kubeapiserver"} {
+				defer func() {
+					e2e.Logf("Restoring %s/cluster's spec", kind)
+					err := oc.WithoutNamespace().Run("patch").Args(kind, "cluster", "--type=json", "-p", patchYamlToRestore).Execute()
+					o.Expect(err).NotTo(o.HaveOccurred())
+				}()
+				g.By("Forcing " + kind + " encryption")
+				err := oc.WithoutNamespace().Run("patch").Args(kind, "cluster", "--type=merge", "-p", patchYaml).Execute()
+				o.Expect(err).NotTo(o.HaveOccurred())
+			}
+
+			newOASEncSecretName := "encryption-key-openshift-apiserver-" + strconv.Itoa(oasEncNumber+1)
+			newKASEncSecretName := "encryption-key-openshift-kube-apiserver-" + strconv.Itoa(kasEncNumber+1)
+
+			g.By("Check the new encryption key secrets appear")
+			err = wait.Poll(5*time.Second, 60*time.Second, func() (bool, error) {
+				output, err := oc.WithoutNamespace().Run("get").Args("secrets", newOASEncSecretName, newKASEncSecretName, "-n", "openshift-config-managed").Output()
+				if err != nil {
+					e2e.Logf("Fail to get new encryption key secrets, error: %s. Trying again", err)
+					return false, nil
+				}
+				e2e.Logf("Got new encryption key secrets:\n%s", output)
+				return true, nil
+			})
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			g.By("Waiting for the force encryption completion")
+			rePattern := regexp.MustCompile(`migrated-resources: .*configmaps.*secrets.*`)
+			// Only need to check kubeapiserver because kubeapiserver takes more time.
+			// In observation, the waiting time can take 25 mins in max, so the Poll parameters are larger.
+			err = wait.Poll(1*time.Minute, 30*time.Minute, func() (bool, error) {
+				output, err := oc.WithoutNamespace().Run("get").Args("secrets", newKASEncSecretName, "-n", "openshift-config-managed", "-o=yaml").Output()
+				if err != nil {
+					e2e.Logf("Fail to get new encryption key secret %s, error: %s. Trying again", newKASEncSecretName, err)
+					return false, nil
+				}
+
+				if matchedStr := rePattern.FindString(output); matchedStr == "" {
+					e2e.Logf("Not yet see migrated-resources. Trying again")
+					return false, nil
+				} else {
+					e2e.Logf("Saw all migrated-resources:\n%s", matchedStr)
+					return true, nil
+				}
+			})
+
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			var oasEncValPrefix2, kasEncValPrefix2 string
+			g.By("Get encryption prefix after force encryption completed")
+			oasEncValPrefix2, err = GetEncryptionPrefix(oc, "/openshift.io/routes")
+			o.Expect(err).NotTo(o.HaveOccurred())
+			e2e.Logf("openshift-apiserver resource encrypted value prefix after test is %s", oasEncValPrefix2)
+
+			kasEncValPrefix2, err = GetEncryptionPrefix(oc, "/kubernetes.io/secrets")
+			o.Expect(err).NotTo(o.HaveOccurred())
+			e2e.Logf("kube-apiserver resource encrypted value prefix after test is %s", kasEncValPrefix2)
+
+			o.Expect(oasEncValPrefix2).Should(o.ContainSubstring("k8s:enc:aescbc:v1"))
+			o.Expect(kasEncValPrefix2).Should(o.ContainSubstring("k8s:enc:aescbc:v1"))
+			o.Expect(oasEncValPrefix2).NotTo(o.Equal(oasEncValPrefix1))
+			o.Expect(kasEncValPrefix2).NotTo(o.Equal(kasEncValPrefix1))
+		} else {
+			g.By("cluster is Etcd Encryption Off, this case intentionally runs nothing")
+		}
+	})
+
 })
---
+++ test/extended/apiserver_and_auth/apiserver_util.go
@@ -0,0 +1,61 @@
+package apiserver_and_auth
+import (
+	"time"
+	"regexp"
+	"strconv"
+	"strings"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+)
+
+func GetEncryptionPrefix(oc *exutil.CLI, key string) (string, error) {
+	var etcdPodName string
+	err := wait.Poll(5*time.Second, 60*time.Second, func() (bool, error) {
+		podName, err := oc.WithoutNamespace().Run("get").Args("pods", "-n", "openshift-etcd", "-l=etcd", "-o=jsonpath={.items[0].metadata.name}").Output()
+		if err != nil {
+			e2e.Logf("Fail to get etcd pod, error: %s. Trying again", err)
+			return false, nil
+		}
+		etcdPodName = podName
+		return true, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	var encryptionPrefix string
+	err = wait.Poll(5*time.Second, 60*time.Second, func() (bool, error) {
+		prefix, err := oc.WithoutNamespace().Run("rsh").Args("-n", "openshift-etcd", "-c", "etcd", etcdPodName, "bash", "-c", `etcdctl get ` + key + ` --prefix -w fields | grep -e "Value" | grep -o k8s:enc:aescbc:v1:[^:]*: | head -n 1`).Output()
+		if err != nil {
+			e2e.Logf("Fail to rsh into etcd pod, error: %s. Trying again", err)
+			return false, nil
+		}
+		encryptionPrefix = prefix
+		return true, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	return encryptionPrefix, nil
+}
+
+func GetEncryptionKeyNumber(oc *exutil.CLI, patten string) (int, error) {
+	secretNames, err := oc.WithoutNamespace().Run("get").Args("secrets", "-n", "openshift-config-managed", `-o=jsonpath={.items[*].metadata.name}`, "--sort-by=metadata.creationTimestamp").Output()
+	if err != nil {
+		e2e.Logf("Fail to get secret, error: %s", err)
+		return 0, nil
+	}
+	rePattern := regexp.MustCompile(patten)
+	locs := rePattern.FindAllStringIndex(secretNames, -1)
+	i, j := locs[len(locs) - 1][0], locs[len(locs) - 1][1]
+	maxSecretName := secretNames[i:j]
+	strSlice := strings.Split(maxSecretName, "-")
+	var number int
+	number, err = strconv.Atoi(strSlice[len(strSlice)-1])
+	if err != nil {
+		e2e.Logf("Fail to get secret, error: %s", err)
+		return 0, nil
+	}
+	return number, nil
+}
