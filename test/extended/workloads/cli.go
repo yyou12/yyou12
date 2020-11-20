3f226c8675dcabc00429547925db43fbcc7fb90e
Ying Zhou <yinzhou@redhat.com>
2020-11-20 16:08:52 +0800

    auto for OCP-28007 (#239)

---
+++ test/extended/workloads/cli.go
@@ -0,0 +1,63 @@
+package workloads
+
+import (
+	"encoding/json"
+	"regexp"
+
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+
+	exutil "github.com/openshift/openshift-tests/test/extended/util"
+)
+
+var _ = g.Describe("[sig-cli] Workloads", func() {
+	defer g.GinkgoRecover()
+
+	var (
+		oc = exutil.NewCLI("oc", exutil.KubeConfigPath())
+	)
+
+	g.It("Medium-28007-Checking oc version show clean as gitTreeState value", func() {
+		out, err := oc.Run("version").Args("-o", "json").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		versionInfo := &VersionInfo{}
+		if err := json.Unmarshal([]byte(out), &versionInfo); err != nil {
+			e2e.Failf("unable to decode version with error: %v", err)
+		}
+		if match, _ := regexp.MatchString("clean", versionInfo.ClientInfo.GitTreeState); !match {
+			e2e.Failf("varification GitTreeState with error: %v", err)
+		}
+
+	})
+})
+
+type ClientVersion struct {
+	BuildDate    string `json:"buildDate"`
+	Compiler     string `json:"compiler"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	GitVersion   string `json:"gitVersion"`
+	GoVersion    string `json:"goVersion"`
+	Major        string `json:"major"`
+	Minor        string `json:"minor"`
+	Platform     string `json:"platform"`
+}
+
+type ServerVersion struct {
+	BuildDate    string `json:"buildDate"`
+	Compiler     string `json:"compiler"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	GitVersion   string `json:"gitVersion"`
+	GoVersion    string `json:"goVersion"`
+	Major        string `json:"major"`
+	Minor        string `json:"minor"`
+	Platform     string `json:"platform"`
+}
+
+type VersionInfo struct {
+	ClientInfo       ClientVersion `json:"ClientVersion"`
+	OpenshiftVersion string        `json:"openshiftVersion"`
+	ServerInfo       ServerVersion `json:"ServerVersion"`
+}
