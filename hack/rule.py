25c4e1f8f49340f9f941b8597f11da73dadab37b
Juan Osorio Robles <jaosorior@redhat.com>
2020-11-24 14:49:02 +0200

    hack/rule.py: Allow running with python 3 (#261)
    Python 2 has been deprecated for a long time. Let's use python 3
    instead.
    
    So this:
    
    * Removes the instances of `print` statements without parentheses
    * Adds the appropriate sheebang statement on the top of the file.

--- hack/rule.py
+++ hack/rule.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import re, os
 
 content = os.popen('git show $(git rev-parse HEAD)', 'r').read()
-print content
+print(content)
 
 sigName = "sig-arch,sig-isc,sig-api-machinery,sig-auth,sig-apps,sig-cli,sig-scheduling,sig-etcd,sig-network,sig-network-edge,sig-storage,sig-openshift-logging,sig-devex, sig-builds,sig-ui,sig-instrumentation,sig-service-catalog,sig-operators,sig-imageregistry,sig-service-catalog,sig-hive,sig-windows,sig-testing,sig-scalability,sig-node,sig-node,sig-cluster-lifecycle,sig-node"
 sigNameList = sigName.replace(' ', '').split(",")
@@ -17,8 +18,8 @@ patternIt = re.compile('\+\s+g.It\(\".*\"')
 itContent = patternIt.findall(content)
 desContent = patternDescribe.findall(content)
 
-print "Des:", desContent
-print "it:", itContent
+print("Des:", desContent)
+print("it:", itContent)
 
 findSig = False
 findTeam = False
@@ -28,19 +29,19 @@ for des in desContent:
 	for sig in sigNameList:
 		if sig in des[1]:
 			findSig = True
-			print "Description: %s, sigName: %s" % (des, sig)
+			print("Description: %s, sigName: %s" % (des, sig))
 			break
 	# des[2] stores the sub team
 	for team in subTeamList:
 		if team in des[2]:
 			findTeam = True
-			print "Description: %s, teamNamee: %s" % (des, team)
+			print("Description: %s, teamNamee: %s" % (des, team))
 			break
 	
 	if findSig and findTeam:
-		print "PASS! sig name: %s | sub team: %s" % (des[1], des[2])
+		print("PASS! sig name: %s | sub team: %s" % (des[1], des[2]))
 	else:
-		print "FAIL! sig or team name doesn't exist: %s" % (des[0],)
+		print("FAIL! sig or team name doesn't exist: %s" % (des[0],))
 		raise Exception("please check the sig or team name above")
 
 if len(itContent) > 0:
@@ -50,9 +51,9 @@ if len(itContent) > 0:
 		if len(itMode) > 0:
 			for imp in importance:
 				if imp.lower() in itMode[0][0].lower():
-					print "FAIL! please follow the naming rule: %s" % (it)
+					print("FAIL! please follow the naming rule: %s" % (it))
 					raise Exception("please check g.It title name above") 
-			print "PASS! title name looks good!", it
+			print("PASS! title name looks good!", it)
 		else:
-			print "FAIL! please follow the naming rule: %s" % (it)
+			print("FAIL! please follow the naming rule: %s" % (it))
 			raise Exception("please check g.It title name above") 
