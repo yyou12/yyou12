a7d7895417389f11bc5fd89407d2edd28500b945
Jian Zhang <jiazha@redhat.com>
2021-03-08 11:22:52 +0800

    fix PR test issue (#577)

--- hack/pr.py
+++ hack/pr.py
@@ -5,19 +5,20 @@ import re, os
 # get the updated content
 # ToDo: to infer the test case ID when the test case content updates, not the title update
 content = os.popen('git show -m', 'r').read()
-# content = '+   g.It("ConnectedOnly-High-37826-23170-20979-use an PullSecret for the private Catalog Source image [Serial]", func() {'
+# content = '+   g.It("ConnectedOnly-High-37826-Low-23170-Medium-20979-High-37442-use an PullSecret for the private Catalog Source image [Serial]", func() {'
 print "=======updated content========"
 print content
 print "=======updated content========"
 
 # get the test case IDs
-patternIt = re.compile('\+\s+g.It\(\".*?((\-\d+)+)')
+patternIt = re.compile('\+\s+g.It\(\".*?((\-[A-Za-z]+\-\d+)+)')
 itContent = patternIt.findall(content)
 print "=======get the content via RE========"
 print itContent
 print "=======get the content via RE========"
-if len(itContent) > 0:
-    testcaseIDs = str(itContent[0][0]).strip("-").replace("-", "|")
+if len(itContent[0]) > 0:
+    testcaseIDs = filter(lambda ch: ch in '0123456789-', str(itContent[0][0]).strip("-"))
+    testcaseIDs = str(testcaseIDs).strip("-").replace("--", "|")  
     # run test case
     commands = './bin/extended-platform-tests run all --dry-run |grep -E '+ '"%s"' % (testcaseIDs) + ' |./bin/extended-platform-tests run -f -'
     print commands
