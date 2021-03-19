a95e81df28d6f7014830ca5b0c322bdde1e63872
Kui Wang <60137115+kuiwang02@users.noreply.github.com>
2021-03-19 13:29:41 +0800

    add Prod example case (#649)

--- test/extended/operators/olm.go
+++ test/extended/operators/olm.go
@@ -2220,7 +2220,7 @@ var _ = g.Describe("[sig-operators] OLM for an end user handle common object", f
 	})
 
 	// It will cover test case: OCP-24076, author: kuiwang@redhat.com
-	g.It("StagerunBoth-Author:kuiwang-Medium-24076-check the version of olm operator is appropriate in ClusterOperator", func() {
+	g.It("ProdrunBoth-StagerunBoth-Author:kuiwang-Medium-24076-check the version of olm operator is appropriate in ClusterOperator", func() {
 		var (
 			olmClusterOperatorName = "operator-lifecycle-manager"
 		)
