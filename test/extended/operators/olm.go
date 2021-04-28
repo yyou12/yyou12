6beb608f7e0335e12c00ca3609431dbea0b40dcc
Xia Zhao <77608951+Xia-Zhao-rh@users.noreply.github.com>
2021-04-28 20:40:27 +0800

    fix 24771 (#875)

--- test/extended/operators/olm.go
+++ test/extended/operators/olm.go
@@ -821,16 +821,7 @@ var _ = g.Describe("[sig-operators] OLM should", func() {
 		sub.createWithoutCheck(oc, itName, dr)
 
 		g.By("5) The install plan is Failed")
-		var installPlan string
-		waitErr := wait.Poll(3*time.Second, 240*time.Second, func() (bool, error) {
-			installPlan, err = oc.AsAdmin().WithoutNamespace().Run("get").Args("sub", sub.subName, "-n", sub.namespace, "-o=jsonpath={.status.installplan.name}").Output()
-			if strings.Compare(installPlan, "") == 0 || err != nil {
-				return false, nil
-			}
-			return true, nil
-		})
-		o.Expect(waitErr).NotTo(o.HaveOccurred())
-		o.Expect(installPlan).NotTo(o.BeEmpty())
+		installPlan := sub.getIP(oc)
 		newCheck("expect", asAdmin, withoutNamespace, compare, "Failed", ok, []string{"ip", installPlan, "-n", sub.namespace, "-o=jsonpath={.status.phase}"}).check(oc)
 
 		g.By("6) Grant the proper permissions to the service account")
