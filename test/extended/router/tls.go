32156d874de245647e6348d583672725852632a3
Hongan Li <hongli@redhat.com>
2021-09-06 15:44:36 +0800

    auto OCP-43301 (#1512)

--- test/extended/router/tls.go
+++ test/extended/router/tls.go
@@ -52,4 +52,44 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		o.Expect(env).To(o.ContainSubstring(`ROUTER_MUTUAL_TLS_AUTH=optional`))
 		o.Expect(env).To(o.ContainSubstring(`ROUTER_MUTUAL_TLS_AUTH_CA=/etc/pki/tls/client-ca/ca-bundle.pem`))
 	})
+
+	// author: hongli@redhat.com
+	g.It("Author:hongli-Medium-43301-enable client certificate with required policy", func() {
+		buildPruningBaseDir := exutil.FixturePath("testdata", "router")
+		customTemp := filepath.Join(buildPruningBaseDir, "ingresscontroller-np.yaml")
+		cmFile := filepath.Join(buildPruningBaseDir, "ca-bundle.pem")
+		var (
+			ingctrl = ingctrlNodePortDescription{
+				name:      "ocp43301",
+				namespace: "openshift-ingress-operator",
+				domain:    "",
+				template:  customTemp,
+			}
+		)
+
+		g.By("create configmap client-ca-xxxxx in namespace openshift-config")
+		createConfigMapFromFile(oc, "openshift-config", "client-ca-43301", cmFile)
+		defer deleteConfigMap(oc, "openshift-config", "client-ca-43301")
+
+		g.By("create custom ingresscontroller")
+		baseDomain := getBaseDomain(oc)
+		ingctrl.domain = ingctrl.name + "." + baseDomain
+		ingctrl.create(oc)
+		defer ingctrl.delete(oc)
+		err := waitForCustomIngressControllerAvailable(oc, ingctrl.name)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("patch the ingresscontroller to enable client certificate with required policy")
+		routerpod := getRouterPod(oc, "ocp43301")
+		patchResourceAsAdmin(oc, ingctrl.namespace, "ingresscontroller/ocp43301", "{\"spec\":{\"clientTLS\":{\"clientCA\":{\"name\":\"client-ca-43301\"},\"clientCertificatePolicy\":\"Required\",\"allowedSubjectPatterns\":[\"www.test2.com\"]}}}")
+		err = waitForResourceToDisappear(oc, "openshift-ingress", "pod/"+routerpod)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("check client certification config after custom router rolled out")
+		newrouterpod := getRouterPod(oc, "ocp43301")
+		env := readRouterPodEnv(oc, newrouterpod, "ROUTER_MUTUAL_TLS_AUTH")
+		o.Expect(env).To(o.ContainSubstring(`ROUTER_MUTUAL_TLS_AUTH=required`))
+		o.Expect(env).To(o.ContainSubstring(`ROUTER_MUTUAL_TLS_AUTH_CA=/etc/pki/tls/client-ca/ca-bundle.pem`))
+		o.Expect(env).To(o.ContainSubstring(`ROUTER_MUTUAL_TLS_AUTH_FILTER=(?:www.test2.com)`))
+	})
 })
