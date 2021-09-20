98014255ac535924def62af12e0fdfc20e566b66
Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>
2021-09-20 14:39:42 -0400

    Auto OCP-29789 (#1635)
    * Auto OCP-29789
    
    * Changes from feedback
    
    * Additional changes based on feedback
    
    * Fixed namespace issue

---
+++ test/extended/psap/nto.go
@@ -0,0 +1,112 @@
+package psap
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
+	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+	"k8s.io/apimachinery/pkg/util/wait"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+)
+
+var _ = g.Describe("[sig-node] PSAP should", func() {
+	defer g.GinkgoRecover()
+
+	var (
+		oc                  = exutil.NewCLI("nto-test", exutil.KubeConfigPath())
+		machineNTONamespace = "openshift-cluster-node-tuning-operator"
+		override_file       = exutil.FixturePath("testdata", "psap", "override.yaml")
+		isNTO               bool
+	)
+
+	g.BeforeEach(func() {
+		// ensure NTO operator is installed
+		isNTO = isNTOInstalled(oc, machineNTONamespace)
+	})
+
+	// author: nweinber@redhat.com
+	g.It("Author:nweinber-Medium-29789-Sysctl parameters set by tuned can not be overwritten by parameters set via /etc/sysctl", func() {
+
+		// test requires NTO to be installed
+		if !isNTO {
+			g.Skip("NTO is not installed - skipping test ...")
+		}
+
+		g.By("Pick one worker node and one tuned pod on said node")
+		workerNodeName, err := exutil.GetFirstWorkerNode(oc)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		e2e.Logf("Worker Node: %v", workerNodeName)
+		tunedPod, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("pods", "-n", machineNTONamespace, "--field-selector=spec.nodeName="+workerNodeName, "-o", "name").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		e2e.Logf("Tuned Pod: %v", tunedPod)
+
+		g.By("Check values set by /etc/sysctl on node and store the values")
+		inotify, err := oc.AsAdmin().WithoutNamespace().Run("debug").Args("-n", machineNTONamespace, "node/"+workerNodeName, "--", "chroot", "/host", "/bin/bash", "-c", "cat /etc/sysctl.d/inotify.conf").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(inotify).To(o.ContainSubstring("fs.inotify.max_user_watches"))
+		o.Expect(inotify).To(o.ContainSubstring("fs.inotify.max_user_instances"))
+		max_user_watches_value := getMaxUserWatchesValue(inotify)
+		max_user_instances_value := getMaxUserInstancesValue(inotify)
+		e2e.Logf("fs.inotify.max_user_watches has value of: %v", max_user_watches_value)
+		e2e.Logf("fs.inotify.max_user_instances has value of: %v", max_user_instances_value)
+
+		g.By("Mount /etc/sysctl on node")
+		err = oc.AsAdmin().WithoutNamespace().Run("exec").Args("-n", machineNTONamespace, tunedPod, "--", "mount").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("Check sysctl kernel.pid_max on node and store the value")
+		kernel, err := oc.AsAdmin().WithoutNamespace().Run("debug").Args("-n", machineNTONamespace, "node/"+workerNodeName, "--", "chroot", "/host", "/bin/bash", "-c", "sysctl kernel.pid_max").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(kernel).To(o.ContainSubstring("kernel.pid_max"))
+		pid_max_value := getKernelPidMaxValue(kernel)
+		e2e.Logf("kernel.pid_max has value of: %v", pid_max_value)
+
+		defer func() {
+			g.By("Removed tuned override and label after test completion")
+			err := oc.AsAdmin().WithoutNamespace().Run("delete").Args("-n", machineNTONamespace, "tuneds.tuned.openshift.io", "override").Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+			err = oc.AsAdmin().WithoutNamespace().Run("label").Args("node", workerNodeName, "tuned.openshift.io/override-").Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+		}()
+
+		g.By("Create new CR and label the node")
+		err = oc.AsAdmin().WithoutNamespace().Run("create").Args("-f", override_file).Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		err = oc.AsAdmin().WithoutNamespace().Run("label").Args("node", workerNodeName, "tuned.openshift.io/override=").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("Check if new NTO profile was applied")
+		err = wait.Poll(5*time.Second, 30*time.Second, func() (bool, error) {
+			podLogs, err := oc.AsAdmin().WithoutNamespace().Run("logs").Args("-n", machineNTONamespace, "--tail=9", tunedPod).Output()
+			o.Expect(err).NotTo(o.HaveOccurred())
+
+			isApplied := strings.Contains(podLogs, "tuned.daemon.daemon: static tuning from profile 'override' applied")
+			if !isApplied {
+				e2e.Logf("Profile has not yet been applied to %v - retrying...", tunedPod)
+				return false, nil
+			}
+			e2e.Logf("Profile has been applied to %v - continuing...", tunedPod)
+			return true, nil
+		})
+		exutil.AssertWaitPollNoErr(err, fmt.Sprintf("Profile was not applied to %v within timeout limit (30 seconds)", tunedPod))
+
+		g.By("Check value of fs.inotify.max_user_instances on node (set by sysctl, should be the same as before)")
+		instanceCheck, err := oc.AsAdmin().WithoutNamespace().Run("debug").Args("-n", machineNTONamespace, "node/"+workerNodeName, "--", "chroot", "/host", "/bin/bash", "-c", "sysctl fs.inotify.max_user_instances").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(instanceCheck).To(o.ContainSubstring(max_user_instances_value))
+
+		g.By("Check value of fs.inotify.max_user_watches on node (set by sysctl, should be the same as before)")
+		watchesCheck, err := oc.AsAdmin().WithoutNamespace().Run("debug").Args("-n", machineNTONamespace, "node/"+workerNodeName, "--", "chroot", "/host", "/bin/bash", "-c", "sysctl fs.inotify.max_user_watches").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(watchesCheck).To(o.ContainSubstring(max_user_watches_value))
+
+		g.By("Check value of kernel.pid_max on node (set by override tuned, should be different than before)")
+		pidCheck, err := oc.AsAdmin().WithoutNamespace().Run("debug").Args("-n", machineNTONamespace, "node/"+workerNodeName, "--", "chroot", "/host", "/bin/bash", "-c", "sysctl kernel.pid_max").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(pidCheck).To(o.ContainSubstring("kernel.pid_max = 1048576"))
+
+	})
+})
--- test/extended/psap/util.go
+++ test/extended/psap/util.go
@@ -1,6 +1,8 @@
 package psap
 
 import (
+	"regexp"
+
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,6 +22,19 @@ func isNFDInstalled(oc *exutil.CLI, machineNFDNamespace string) bool {
 	return true
 }
 
+// isNTOInstalled will return false if NTO is not installed, and true otherwise
+func isNTOInstalled(oc *exutil.CLI, machineNTONamespace string) bool {
+	e2e.Logf("Checking if NTO is installed ...")
+	podList, err := oc.AdminKubeClient().CoreV1().Pods(machineNTONamespace).List(metav1.ListOptions{})
+	o.Expect(err).NotTo(o.HaveOccurred())
+	if len(podList.Items) == 0 {
+		e2e.Logf("NTO not found :(")
+		return false
+	}
+	e2e.Logf("NTO found!")
+	return true
+}
+
 // createYAMLFromMachineSet creates a YAML file with a given filename from a given machineset name in a given API namespace, throws an error if creation fails
 func createYAMLFromMachineSet(oc *exutil.CLI, machineAPINamespace string, machineSetName string, filename string) (string, error) {
 	machineset_yaml, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("machineset", "-n", machineAPINamespace, machineSetName, "-o", "yaml").OutputToFile(filename)
@@ -29,4 +44,28 @@ func createYAMLFromMachineSet(oc *exutil.CLI, machineAPINamespace string, machin
 // createMachineSetFromYAML creates a new machineset from the YAML configuration in a given filename, throws an error if creation fails
 func createMachineSetFromYAML(oc *exutil.CLI, filename string) error {
 	return oc.AsAdmin().WithoutNamespace().Run("create").Args("-f", filename).Execute()
+}
+
+func getMaxUserWatchesValue(inotify string) string {
+	re_line := regexp.MustCompile(`fs.inotify.max_user_watches = \d+`)
+	re_value := regexp.MustCompile(`\d+`)
+	max_user_watches := re_line.FindString(inotify)
+	max_user_watches_value := re_value.FindString(max_user_watches)
+	return max_user_watches_value
+}
+
+func getMaxUserInstancesValue(inotify string) string {
+	re_line := regexp.MustCompile(`fs.inotify.max_user_instances = \d+`)
+	re_value := regexp.MustCompile(`\d+`)
+	max_user_instances := re_line.FindString(inotify)
+	max_user_instances_value := re_value.FindString(max_user_instances)
+	return max_user_instances_value
+}
+
+func getKernelPidMaxValue(kernel string) string {
+	re_line := regexp.MustCompile(`kernel.pid_max = \d+`)
+	re_value := regexp.MustCompile(`\d+`)
+	pid_max := re_line.FindString(kernel)
+	pid_max_value := re_value.FindString(pid_max)
+	return pid_max_value
 }
