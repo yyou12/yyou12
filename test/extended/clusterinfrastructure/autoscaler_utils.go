74fa191fe692cd647dc3cb31b57095c981d8c567
Zhaohua Sun <zhsun@redhat.com>
2021-10-09 10:01:35 +0800

    move some functions to a common util (#1851)

--- test/extended/clusterinfrastructure/autoscaler_utils.go
+++ test/extended/clusterinfrastructure/autoscaler_utils.go
@@ -1,19 +1,11 @@
 package clusterinfrastructure
 
 import (
-	"math/rand"
-	"strconv"
-	"time"
-
 	o "github.com/onsi/gomega"
+	"strconv"
 
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
-	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
-)
-
-const (
-	machineAPINamespace = "openshift-machine-api"
 )
 
 type clusterAutoscalerDescription struct {
@@ -53,30 +45,4 @@ func (machineAutoscaler *machineAutoscalerDescription) createMachineAutoscaler(o
 func (machineAutoscaler *machineAutoscalerDescription) deleteMachineAutoscaler(oc *exutil.CLI) error {
 	e2e.Logf("Deleting a machineautoscaler ...")
 	return oc.AsAdmin().WithoutNamespace().Run("delete").Args("machineautoscaler", machineAutoscaler.name, "-n", machineAPINamespace).Execute()
-}
-
-func applyResourceFromTemplate(oc *exutil.CLI, parameters ...string) error {
-	var jsonCfg string
-	err := wait.Poll(3*time.Second, 15*time.Second, func() (bool, error) {
-		output, err := oc.AsAdmin().Run("process").Args(parameters...).OutputToFile(getRandomString() + "autoscaler.json")
-		if err != nil {
-			e2e.Failf("the result of ReadFile:%v", err)
-			return false, nil
-		}
-		jsonCfg = output
-		return true, nil
-	})
-	exutil.AssertWaitPollNoErr(err, "Applying resources from template is failed")
-	e2e.Logf("The resource is %s", jsonCfg)
-	return oc.AsAdmin().WithoutNamespace().Run("apply").Args("-f", jsonCfg).Execute()
-}
-
-func getRandomString() string {
-	chars := "abcdefghijklmnopqrstuvwxyz0123456789"
-	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
-	buffer := make([]byte, 8)
-	for index := range buffer {
-		buffer[index] = chars[seed.Intn(len(chars))]
-	}
-	return string(buffer)
 }
