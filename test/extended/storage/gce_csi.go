6cc42ebcf7719810c80234db7840d344a786ffc0
chaoyang <chaoyang@redhat.com>
2021-11-10 12:46:39 +0800

    Auto OCP-37490 (#2040)
    * Auto OCP-37490
    
    * update based on comments
    
    * Update to [Disruptive]

---
+++ test/extended/storage/gce_csi.go
@@ -0,0 +1,90 @@
+package storage
+
+import (
+	"path/filepath"
+	"strings"
+
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
+	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+)
+
+var _ = g.Describe("[sig-storage] STORAGE", func() {
+	defer g.GinkgoRecover()
+
+	var oc = exutil.NewCLI("storage-gcp-csi", exutil.KubeConfigPath())
+
+	// gcp-csi test suite cloud provider support check
+
+	g.BeforeEach(func() {
+		cloudProvider := getCloudProvider(oc)
+		if !strings.Contains(cloudProvider, "gcp") {
+			g.Skip("Skip for non-supported cloud provider!!!")
+		}
+	})
+
+	// author: chaoyang@redhat.com
+	// [GKE-PD-CSI] [Dynamic Regional PV]Check provisioned region pv and drain node function
+	g.It("Author:chaoyang-Critical-37490-[GKE-PD-CSI] Check provisioned region pv and drain node function [Disruptive]", func() {
+		var (
+			storageTeamBaseDir   = exutil.FixturePath("testdata", "storage")
+			storageClassTemplate = filepath.Join(storageTeamBaseDir, "storageclass-template.yaml")
+			pvcTemplate          = filepath.Join(storageTeamBaseDir, "pvc-template.yaml")
+			deploymentTemplate   = filepath.Join(storageTeamBaseDir, "dep-template.yaml")
+			storageClass         = newStorageClass(setStorageClassTemplate(storageClassTemplate), setStorageClassProvisioner("pd.csi.storage.gke.io"))
+
+			pvc = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimStorageClassName(storageClass.name),
+				setPersistentVolumeClaimCapacity("200Gi"))
+
+			dep                    = newDeployment(setDeploymentTemplate(deploymentTemplate), setDeploymentPVCName(pvc.name))
+			storageClassParameters = map[string]string{
+
+				"type":             "pd-standard",
+				"replication-type": "regional-pd"}
+			extraParameters = map[string]interface{}{
+				"parameters":           storageClassParameters,
+				"allowVolumeExpansion": true,
+			}
+		)
+
+		g.By("0. Create new project for the scenario")
+		oc.SetupProject() //create new project
+		pvc.namespace = oc.Namespace()
+		dep.namespace = pvc.namespace
+
+		g.By("1. Create gcp-pd-csi storageclass")
+		storageClass.createWithExtraParameters(oc, extraParameters)
+		defer storageClass.deleteAsAdmin(oc)
+
+		g.By("2. Create a pvc with the gcp-pd-csi storageclass")
+		pvc.create(oc)
+		defer pvc.deleteAsAdmin(oc)
+
+		g.By("3. Create deployment with the created pvc and wait for the pod ready")
+		dep.create(oc)
+		defer dep.deleteAsAdmin(oc)
+
+		g.By("4. Wait for the deployment ready")
+		dep.waitReady(oc)
+		podLabel := "app=" + dep.applabel
+
+		g.By("5. Drain the pod to other nodes")
+		nodeName0 := getNodeNameByPod(oc, dep.namespace, dep.getPodList(oc)[0])
+		nodeZone0, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("nodes/"+nodeName0, "-o=jsonpath={.metadata.labels.topology\\.gke\\.io\\/zone}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		errDrain := oc.AsAdmin().WithoutNamespace().Run("adm").Args("drain", "nodes/"+nodeName0, "--pod-selector", podLabel).Execute()
+		o.Expect(errDrain).NotTo(o.HaveOccurred())
+		defer oc.AsAdmin().WithoutNamespace().Run("adm").Args("uncordon", "nodes/"+nodeName0).Execute()
+
+		g.By("6. Wait for the pod ready again")
+		dep.waitReady(oc)
+		nodeName1 := getNodeNameByPod(oc, dep.namespace, dep.getPodList(oc)[0])
+		nodeZone1, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("nodes/"+nodeName1, "-o=jsonpath={.metadata.labels.topology\\.gke\\.io\\/zone}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("7. Check node0 and node1 are in different zones")
+		o.Expect(nodeZone0).ShouldNot(o.Equal(nodeZone1))
+
+	})
+
+})
