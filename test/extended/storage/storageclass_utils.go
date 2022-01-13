6beb8562eb6ad4fbe18208bfc58fb393066f8133
Wei Duan <wduan@redhat.com>
2022-01-13 09:26:18 +0800

    Automate OCP-22019 (#2429)

--- test/extended/storage/storageclass_utils.go
+++ test/extended/storage/storageclass_utils.go
@@ -1,9 +1,18 @@
 package storage
 
 import (
-	o "github.com/onsi/gomega"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"time"
 
+	g "github.com/onsi/ginkgo"
+	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+	"github.com/tidwall/gjson"
+	"k8s.io/apimachinery/pkg/util/wait"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
 
 type storageClass struct {
@@ -98,4 +107,61 @@ func (sc *storageClass) createWithExtraParameters(oc *exutil.CLI, extraParameter
 func (sc *storageClass) negative() *storageClass {
 	sc.negativeTest = true
 	return sc
+}
+
+// Check if pre-defined storageclass exists
+func preDefinedStorageclassCheck(cloudProvider string) {
+	preDefinedStorageclassMatrix, err := ioutil.ReadFile(filepath.Join(exutil.FixturePath("testdata", "storage", "config"), "pre-defined-storageclass.json"))
+	o.Expect(err).NotTo(o.HaveOccurred())
+	supportPlatformsBool := gjson.GetBytes(preDefinedStorageclassMatrix, "platforms.#(name="+cloudProvider+").storageclass|@flatten").Exists()
+	if !supportPlatformsBool {
+		g.Skip("Skip for no pre-defined storageclass on " + cloudProvider + "!!! Or please check the test configuration")
+	}
+}
+
+// Get default storage class name from pre-defined-storageclass matrix
+func getClusterDefaultStorageclassByPlatform(cloudProvider string) string {
+	preDefinedStorageclassMatrix, err := ioutil.ReadFile(filepath.Join(exutil.FixturePath("testdata", "storage", "config"), "pre-defined-storageclass.json"))
+	o.Expect(err).NotTo(o.HaveOccurred())
+	sc := gjson.GetBytes(preDefinedStorageclassMatrix, "platforms.#(name="+cloudProvider+").storageclass.default_sc").String()
+	e2e.Logf("The default storageclass is: %s.", sc)
+	return sc
+}
+
+// Get pre-defined storage class name list from pre-defined-storageclass matrix
+func getClusterPreDefinedStorageclassByPlatform(cloudProvider string) []string {
+	preDefinedStorageclassMatrix, err := ioutil.ReadFile(filepath.Join(exutil.FixturePath("testdata", "storage", "config"), "pre-defined-storageclass.json"))
+	o.Expect(err).NotTo(o.HaveOccurred())
+	preDefinedStorageclass := []string{}
+	sc := gjson.GetBytes(preDefinedStorageclassMatrix, "platforms.#(name="+cloudProvider+").storageclass.pre_defined_sc").Array()
+	for _, v := range sc {
+		preDefinedStorageclass = append(preDefinedStorageclass, v.Str)
+	}
+	return preDefinedStorageclass
+}
+
+// check storageclass exist in given waitting time
+func checkSrorageclassExists(oc *exutil.CLI, sc string) {
+	err := wait.Poll(5*time.Second, 30*time.Second, func() (bool, error) {
+		output, err1 := oc.AsAdmin().WithoutNamespace().Run("get").Args("sc", sc, "-o", "jsonpath={.metadata.name}").Output()
+		if err1 != nil {
+			e2e.Logf("Get error to get the storageclass %v", sc)
+			return false, nil
+		}
+		if output != sc {
+			return false, nil
+		}
+		return true, nil
+	})
+	exutil.AssertWaitPollNoErr(err, fmt.Sprintf("Could not find the storageclass %v", sc))
+}
+
+// Check if given storageclass is default storageclass
+func checkDefaultStorageclass(oc *exutil.CLI, sc string) bool {
+	stat, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("sc", sc, "-o", "jsonpath={.metadata.annotations.storageclass\\.kubernetes\\.io/is-default-class}").Output()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	output, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("sc").Output()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	debugLogf("oc get sc:\n%s", output)
+	return strings.EqualFold(stat, "true")
 }
