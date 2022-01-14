3c23de398bc492f5d0d0849111b59f056007e13d
Zhaohua Sun <zhsun@redhat.com>
2022-01-14 17:09:58 +0800

    Auto OCP-42657 (#1716)

--- test/extended/clusterinfrastructure/ccm.go
+++ test/extended/clusterinfrastructure/ccm.go
@@ -10,7 +10,7 @@ import (
 var _ = g.Describe("[sig-cluster-lifecycle] Cluster_Infrastructure", func() {
 	defer g.GinkgoRecover()
 	var (
-		oc = exutil.NewCLI("cloud-controller-manager", exutil.KubeConfigPath())
+		oc           = exutil.NewCLI("cloud-controller-manager", exutil.KubeConfigPath())
 		iaasPlatform string
 	)
 
@@ -66,6 +66,40 @@ var _ = g.Describe("[sig-cluster-lifecycle] Cluster_Infrastructure", func() {
 		deploy, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("deploy", "-n", "openshift-cloud-controller-manager", "-o=jsonpath={.items[*].metadata.name}").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
 		o.Expect(deploy).To(o.ContainSubstring("cloud-controller-manager"))
+		g.By("Check if appropriate `--cloud-provider=external` set on kubelet, KAPI and KCM")
+		masterkubelet, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("machineconfig/01-master-kubelet", "-o=jsonpath={.spec.config.systemd.units[0].contents}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(masterkubelet).To(o.ContainSubstring("cloud-provider=external"))
+		workerkubelet, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("machineconfig/01-worker-kubelet", "-o=jsonpath={.spec.config.systemd.units[0].contents}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(workerkubelet).To(o.ContainSubstring("cloud-provider=external"))
+		kapi, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("cm/config", "-n", "openshift-kube-apiserver", "-o=jsonpath={.data.config\\.yaml}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(kapi).To(o.ContainSubstring("\"cloud-provider\":[\"external\"]"))
+		kcm, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("cm/config", "-n", "openshift-kube-controller-manager", "-o=jsonpath={.data.config\\.yaml}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(kcm).To(o.ContainSubstring("\"cloud-provider\":[\"external\"]"))
+	})
+
+	// author: zhsun@redhat.com
+	g.It("Longduration-NonPreRelease-Author:zhsun-High-42657-Enable out-of-tree cloud providers with feature gate [Disruptive]", func() {
+		g.By("Check if ccm on this platform is supported")
+		if !(iaasPlatform == "aws" || iaasPlatform == "azure" || iaasPlatform == "openstack" || iaasPlatform == "gcp" || iaasPlatform == "vsphere") {
+			g.Skip("Skip for ccm on this platform is not supported or don't need to enable!")
+		}
+		g.By("Check if ccm is deployed")
+		ccm, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("deploy", "-n", "openshift-cloud-controller-manager", "-o=jsonpath={.items[*].metadata.name}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		if len(ccm) != 0 {
+			g.Skip("Skip for ccm is already be deployed!")
+		}
+
+		g.By("Enable out-of-tree cloud provider with feature gate")
+		err = oc.AsAdmin().WithoutNamespace().Run("patch").Args("featuregate/cluster", "-p", `{"spec":{"featureSet": "TechPreviewNoUpgrade"}}`, "--type=merge").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		g.By("Check cluster is still healthy")
+		waitForClusterHealthy(oc)
+
 		g.By("Check if appropriate `--cloud-provider=external` set on kubelet, KAPI and KCM")
 		masterkubelet, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("machineconfig/01-master-kubelet", "-o=jsonpath={.spec.config.systemd.units[0].contents}").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
