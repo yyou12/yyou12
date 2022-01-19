3f248fece6c448510612b0cfc9da6a52eb2feb4e
Kui Wang <60137115+kuiwang02@users.noreply.github.com>
2022-01-19 15:51:02 +0800

    correct suite name (#2488)

--- pkg/test/ginkgo/cmd_runsuite.go
+++ pkg/test/ginkgo/cmd_runsuite.go
@@ -314,7 +314,7 @@ func (opt *Options) Run(args []string) error {
 	}
 
 	if len(opt.JUnitDir) > 0 {
-		if err := writeJUnitReport("junit_e2e", "openshift-tests", tests, opt.JUnitDir, duration, opt.ErrOut, syntheticTestResults...); err != nil {
+		if err := writeJUnitReport("junit_e2e", "openshift-tests-private", tests, opt.JUnitDir, duration, opt.ErrOut, syntheticTestResults...); err != nil {
 			fmt.Fprintf(opt.Out, "error: Unable to write e2e JUnit results: %v", err)
 		}
 	}
